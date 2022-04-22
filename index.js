98eeddbebe15f117b545451fc7f06cd10afe565e
nkunic <natasa.kunic@gmail.com>
2022-04-22 18:24:46 +0200

    2 files modified

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ var comp2Btn = document.querySelector('#comp2Btn');
 var comp3Area = document.querySelector('#comp3Area');
 var comp3Btn = document.querySelector('#comp3Btn');
 
-var comp4Area = document.querySelector('#comp4Area');
-var comp4Btn = document.querySelector('#comp4Btn');
-
 var startBtn = document.querySelector('#startBtn');
 var infoArea = document.querySelector('#infoArea');
 var texts = [
-  "Juče je bio divan dan", "Danas je divan dan", "Sutra će biti dian dan"
-]
+  'Juče je bio divan dan',
+  'Danas je divan dan',
+  'Sutra će biti divan dan',
+];
+var rand;
 
 startBtn.addEventListener('click', timer);
 
@@ -37,15 +37,57 @@ function timer() {
       clearInterval(loop);
       // startBtn.style.display = "none";
       startBtn.classList.add('d-none');
-      startTyping();      
+      startTyping();
     }
   }, 1000);
 }
 
 function startTyping() {
-  var rand = Math.floor(Math.random()*texts.length);
+  rand = Math.floor(Math.random() * texts.length);
   infoArea.innerHTML = '<h3>' + texts[rand] + '</h3>';
   infoArea.classList.remove('d-none');
   infoArea.classList.add('d-block');
   playerArea.focus();
+  comp1StartTyping();
+  comp2StartTyping();
+  comp2StartTyping();  
+}
+
+function comp1StartTyping() {
+  var text = texts[rand];
+  console.log(text);
+  var textArray = text.split('');
+  var loop = setInterval(function () {
+    if (textArray.length != 0) {
+      comp1Area.value += textArray.shift();
+    } else {
+      clearInterval(loop);
+    }
+  }, 250);
+}
+
+function comp2StartTyping() {
+  var text = texts[rand];
+  console.log(text);
+  var textArray = text.split('');
+  var loop = setInterval(function () {
+    if (textArray.length != 0) {
+      comp2Area.value += textArray.shift();
+    } else {
+      clearInterval(loop);
+    }
+  }, 500);
+}
+
+function comp3StartTyping() {
+  var text = texts[rand];
+  console.log(text);
+  var textArray = text.split('');
+  var loop = setInterval(function () {
+    if (textArray.length != 0) {
+      comp3Area.value += textArray.shift();
+    } else {
+      clearInterval(loop);
+    }
+  }, 1000);
 }
